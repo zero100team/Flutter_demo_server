890ffec7d4979ef25fb212bf3923968a9ac5ad64
distrue <gil4260@korea.ac.kr>
2020-06-08 19:16:10 +0900

    all sessions on Websocket

--- app.ts
+++ app.ts
@@ -3,12 +3,24 @@ import ws from 'ws';
 
 const wsServer = new ws.Server({ port: 4000 });
 
+var sessions: ws[] = [];
+
 wsServer.on('connection', function connection(ws: ws) {
-    
+    sessions.push(ws);    
     ws.on('message', function incoming(message: string) {
         console.log('received: %s', message);
-        ws.send(message);
-    })
+        sessions.forEach((item: ws) => {
+            item.send(message);
+        });
+    });
+
+    ws.on('close', function close() {
+        sessions.forEach((item: ws, idx: number) => {
+            if(item === ws) {
+                sessions.splice(idx, 1);
+            }
+        });
+    });
     
     ws.send("init");
 })
